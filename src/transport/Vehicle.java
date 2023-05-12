4d9126ea6ed78ad9a57c6f06000b2c63cc28df8f
Kethya <kethyakas@yahoo.com>
2023-05-11 18:38:22 -0700

    updated constructor

--- src/transport/Vehicle.java
+++ src/transport/Vehicle.java
@@ -1,5 +1,14 @@
 package transport;
 
 public class Vehicle {
-	public static void main(String[] args) { Vehicle v = new Vehicle(); }
+	
+	private int nWheels;
+
+	public Vehicle(int wheels) {
+		nWheels = wheels;
+		
+		System.out.println("Vehicle constructor");
+	}
+	
+	public static void main(String[] args) { Vehicle v = new Vehicle(4); }
 }
